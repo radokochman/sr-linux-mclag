d32b6bb1599fe997031bde79a694208c9f81d245
Radoslaw Kochman <radoslaw.kochman@gmail.com>
2025-06-10 21:28:29 +0200

    add assert in strip_returned_value

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,6 +2,9 @@ from tests.models import Interface, Subinterface
 
 
 def strip_returned_value(result: dict) -> str:
+    assert "update" in result["notification"][0].keys(), (
+        "Expected 'update' key in the result, probably requested value is not present"
+    )
     return result["notification"][0]["update"][0]["val"]
 
 
