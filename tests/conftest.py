20cd664adc5cbc93d2525afc47441ee60935ca69
Radoslaw Kochman <radoslaw.kochman@gmail.com>
2025-06-10 21:17:14 +0200

    change order of except entries

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ def gnmi_client(device):
             insecure=True,
         ) as client:
             yield client
-    except gNMIException as e:
-        pytest.fail(f"gNMI client connection failed: {e}")
     except FutureTimeoutError as e:
         pytest.fail(f"gNMI client connection timed out: {e}")
+    except gNMIException as e:
+        pytest.fail(f"gNMI client connection failed: {e}")
